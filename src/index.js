ccc678f15f18f0a365d365e801adc0e16d555017
Dzmitry Sysunovich <Sysunovich@gmail.com>
2019-10-11 23:57:26 +0300

    add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = []; items = ['(', ')', '{', '}', '[', ']', '|', '|', '1', '2', '3', '4', '5', '6', '7', '8'];
+
+    for (let i = 0; i < str.length; i++) {
+        if (items.includes(str[i])) {
+            stack.push(str[i]);
+        } else {
+            if (bracketsConfig.indexOf(str[i]) === str.indexOf(stack[stack.length - 1])) {
+                stack.pop();
+                } else {
+                    return false;
+                }
+        }
+    }
+
+    return stack.length === 0;
 }
